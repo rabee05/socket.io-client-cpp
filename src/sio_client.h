e2268b7a2959d0225a15dc46a0b5e7a41975ecd1
Rabee Zyoud <rabee05@hotmail.com>
2025-10-13 02:34:14 +0000

    style: Improve code formatting and consistency in sio_client.h and sio_socket.h

--- src/sio_client.h
+++ src/sio_client.h
@@ -12,7 +12,8 @@
 #include "sio_message.h"
 #include "sio_socket.h"
 
-namespace asio {
+namespace asio
+{
     class io_context;
 }
 
@@ -20,31 +21,35 @@ namespace sio
 {
     class client_impl;
 
-    struct client_options {
-        asio::io_context* io_context = nullptr;
+    struct client_options
+    {
+        asio::io_context *io_context = nullptr;
     };
 
-    struct reconnect_config {
-        unsigned attempts = 0xFFFFFFFF;  // Infinite by default (max unsigned int)
-        unsigned delay = 5000;            // 5 seconds
-        unsigned delay_max = 25000;       // 25 seconds
+    struct reconnect_config
+    {
+        unsigned attempts = 0xFFFFFFFF; // Infinite by default (max unsigned int)
+        unsigned delay = 5000;          // 5 seconds
+        unsigned delay_max = 25000;     // 25 seconds
         bool enabled = true;
 
         reconnect_config() = default;
 
         // Convenience constructor
         reconnect_config(unsigned attempts_, unsigned delay_, unsigned delay_max_)
-            : attempts(attempts_), delay(delay_), delay_max(delay_), enabled(true) {}
+            : attempts(attempts_), delay(delay_), delay_max(delay_max_), enabled(true) {}
 
         // Disable reconnection
-        static reconnect_config disabled() {
+        static reconnect_config disabled()
+        {
             reconnect_config config;
             config.enabled = false;
             return config;
         }
     };
-    
-    class client {
+
+    class client
+    {
     public:
         enum close_reason
         {
@@ -62,54 +67,54 @@ namespace sio
         };
 
         typedef std::function<void(void)> con_listener;
-        
-        typedef std::function<void(close_reason const& reason)> close_listener;
+
+        typedef std::function<void(close_reason const &reason)> close_listener;
 
         typedef std::function<void(unsigned, unsigned)> reconnect_listener;
 
         typedef std::function<void(connection_state)> state_listener;
 
-        typedef std::function<void(std::string const& nsp)> socket_listener;
-        
+        typedef std::function<void(std::string const &nsp)> socket_listener;
+
         client();
-        client(client_options const& options);
+        client(client_options const &options);
         ~client();
-        
-        //set listeners and event bindings.
-        void set_open_listener(con_listener const& l);
-        
-        void set_fail_listener(con_listener const& l);
-        
-        void set_reconnecting_listener(con_listener const& l);
 
-        void set_reconnect_listener(reconnect_listener const& l);
+        // set listeners and event bindings.
+        void set_open_listener(con_listener const &l);
 
-        void set_close_listener(close_listener const& l);
-        
-        void set_socket_open_listener(socket_listener const& l);
-        
-        void set_socket_close_listener(socket_listener const& l);
+        void set_fail_listener(con_listener const &l);
 
-        void set_state_listener(state_listener const& l);
+        void set_reconnecting_listener(con_listener const &l);
+
+        void set_reconnect_listener(reconnect_listener const &l);
+
+        void set_close_listener(close_listener const &l);
+
+        void set_socket_open_listener(socket_listener const &l);
+
+        void set_socket_close_listener(socket_listener const &l);
+
+        void set_state_listener(state_listener const &l);
 
         void clear_con_listeners();
 
         void clear_socket_listeners();
-        
+
         // Client Functions - such as send, etc.
-        void connect(const std::string& uri);
+        void connect(const std::string &uri);
 
-        void connect(const std::string& uri, const message::ptr& auth);
+        void connect(const std::string &uri, const message::ptr &auth);
 
-        void connect(const std::string& uri, const std::map<std::string,std::string>& query);
+        void connect(const std::string &uri, const std::map<std::string, std::string> &query);
 
-        void connect(const std::string& uri, const std::map<std::string,std::string>& query, const message::ptr& auth);
+        void connect(const std::string &uri, const std::map<std::string, std::string> &query, const message::ptr &auth);
 
-        void connect(const std::string& uri, const std::map<std::string,std::string>& query,
-                     const std::map<std::string,std::string>& http_extra_headers);
+        void connect(const std::string &uri, const std::map<std::string, std::string> &query,
+                     const std::map<std::string, std::string> &http_extra_headers);
 
-        void connect(const std::string& uri, const std::map<std::string,std::string>& query,
-                     const std::map<std::string,std::string>& http_extra_headers, const message::ptr& auth);
+        void connect(const std::string &uri, const std::map<std::string, std::string> &query,
+                     const std::map<std::string, std::string> &http_extra_headers, const message::ptr &auth);
 
         void set_reconnect_attempts(int attempts);
 
@@ -118,7 +123,7 @@ namespace sio
         void set_reconnect_delay_max(unsigned millis);
 
         // Convenient method to configure all reconnection parameters at once
-        void set_reconnect_config(const reconnect_config& config);
+        void set_reconnect_config(const reconnect_config &config);
 
         void set_logs_default();
 
@@ -126,33 +131,33 @@ namespace sio
 
         void set_logs_verbose();
 
-        sio::socket::ptr const& socket(const std::string& nsp = "");
-        
+        sio::socket::ptr const &socket(const std::string &nsp = "");
+
         // Closes the connection
         void close();
-        
+
         void sync_close();
-        
-        void set_proxy_basic_auth(const std::string& uri, const std::string& username, const std::string& password);
-		
+
+        void set_proxy_basic_auth(const std::string &uri, const std::string &username, const std::string &password);
+
         bool opened() const;
 
         connection_state get_connection_state() const;
 
-        std::string const& get_sessionid() const;
-        
-    private:
-        //disable copy constructor and assign operator.
-        client(client const&){}
-        void operator=(client const&){}
+        std::string const &get_sessionid() const;
 
-        struct impl_deleter {
-            void operator()(client_impl* p) const;
+    private:
+        // disable copy constructor and assign operator.
+        client(client const &) {}
+        void operator=(client const &) {}
+
+        struct impl_deleter
+        {
+            void operator()(client_impl *p) const;
         };
         std::unique_ptr<client_impl, impl_deleter> m_impl;
     };
-    
-}
 
+}
 
 #endif // __SIO_CLIENT__H__
--- src/sio_socket.h
+++ src/sio_socket.h
@@ -6,106 +6,115 @@
 namespace sio
 {
     class event_adapter;
-    
+
     class event
     {
     public:
-        const std::string& get_nsp() const;
-        
-        const std::string& get_name() const;
-        
-        const message::ptr& get_message() const;
+        const std::string &get_nsp() const;
 
-        const message::list& get_messages() const;
-        
+        const std::string &get_name() const;
+
+        const message::ptr &get_message() const;
+
+        const message::list &get_messages() const;
+
         bool need_ack() const;
-        
-        void put_ack_message(message::list const& ack_message);
-        
-        message::list const& get_ack_message() const;
-        
-    protected:
-        event(std::string const& nsp,std::string const& name,message::list const& messages,bool need_ack);
-        event(std::string const& nsp,std::string const& name,message::list&& messages,bool need_ack);
 
-        message::list& get_ack_message_impl();
-        
+        void put_ack_message(message::list const &ack_message);
+
+        message::list const &get_ack_message() const;
+
+    protected:
+        event(std::string const &nsp, std::string const &name, message::list const &messages, bool need_ack);
+        event(std::string const &nsp, std::string const &name, message::list &&messages, bool need_ack);
+
+        message::list &get_ack_message_impl();
+
     private:
         const std::string m_nsp;
         const std::string m_name;
         const message::list m_messages;
         const bool m_need_ack;
         message::list m_ack_message;
-        
+
         friend class event_adapter;
     };
-    
+
     class client_impl;
     class packet;
-    
-    //The name 'socket' is taken from concept of official socket.io.
+
+    // The name 'socket' is taken from concept of official socket.io.
     class socket
     {
     public:
-        typedef std::function<void(const std::string& name,message::ptr const& message,bool need_ack, message::list& ack_message)> event_listener_aux;
+        typedef std::function<void(const std::string &name, message::ptr const &message, bool need_ack, message::list &ack_message)> event_listener_aux;
 
-        typedef std::function<void(event& event)> event_listener;
+        typedef std::function<void(event &event)> event_listener;
 
-        typedef std::function<bool(message::ptr const& message)> simple_event_handler;
+        // Backward-compatible simple handler (returns bool to indicate success)
+        typedef std::function<bool(message::ptr const &message)> simple_event_handler;
 
-        typedef std::function<void(message::ptr const& message)> error_listener;
-        
+        // New handler signature that provides ack_message to populate
+        typedef std::function<void(message::ptr const &message, message::list &ack_message)> event_handler_with_ack;
+
+        typedef std::function<void(message::ptr const &message)> error_listener;
+
         typedef std::shared_ptr<socket> ptr;
 
         ~socket();
-        
-        void on(std::string const& event_name,event_listener const& func);
 
-        void on(std::string const& event_name,event_listener_aux const& func);
+        void on(std::string const &event_name, event_listener const &func);
 
-        void on_with_ack(std::string const& event_name, simple_event_handler const& handler);
+        void on(std::string const &event_name, event_listener_aux const &func);
 
-        void off(std::string const& event_name);
-        
-        void on_any(event_listener const& func);
+        // New API: handler can fill ack payload directly
+        void on_with_ack(std::string const &event_name, event_handler_with_ack const &handler);
 
-        void on_any(event_listener_aux const& func);
+        // Old API: handler returns success boolean; we translate to ack [true/false]
+        void on_with_ack(std::string const &event_name, simple_event_handler const &handler);
+
+        void off(std::string const &event_name);
+
+        void on_any(event_listener const &func);
+
+        void on_any(event_listener_aux const &func);
 
         void off_all();
-        
+
         void close();
-        
-        void on_error(error_listener const& l);
-        
+
+        void on_error(error_listener const &l);
+
         void off_error();
 
-        void emit(std::string const& name, message::list const& msglist = nullptr, std::function<void (message::list const&)> const& ack = nullptr);
-        
-        std::string const& get_namespace() const;
-        
+        void emit(std::string const &name, message::list const &msglist = nullptr, std::function<void(message::list const &)> const &ack = nullptr);
+
+        std::string const &get_namespace() const;
+
     protected:
-        socket(client_impl*,std::string const&,message::ptr const&);
+        socket(client_impl *, std::string const &, message::ptr const &);
 
         void on_connected();
-        
+
         void on_close();
-        
+
         void on_open();
-        
+
         void on_disconnect();
-        
-        void on_message_packet(packet const& p);
-        
+
+        void on_message_packet(packet const &p);
+
         friend class client_impl;
-        
+
     private:
-        //disable copy constructor and assign operator.
-        socket(socket const&){}
-        void operator=(socket const&){}
+        // disable copy constructor and assign operator.
+        socket(socket const &) {}
+        void operator=(socket const &) {}
 
         class impl;
-        struct impl_deleter {
-            void operator()(impl* p) const;
+        struct impl_deleter
+        {
+            void operator()(impl *p) const;
         };
         std::unique_ptr<impl, impl_deleter> m_impl;
     };
